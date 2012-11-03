3cf81ce234401c0eb972b005b3d70a29ea3bea68
Joy <sweetcherry77@gmail.com>
2012-11-03 12:21:09 -0400

    Added Data class

---
+++ src/BackEnd/Analysis.java
@@ -0,0 +1,33 @@
+package BackEnd;
+
+/**
+ * Given an array of data from one round,
+ * prints out the relevant information needed
+ * so User can see how to improve on their skills.
+ */
+public class Analysis {
+	Data[] data;
+	long total; //total time
+	int num; //number of problems
+	
+	int mean(){
+		long sum = 0;
+		for(Data d: data){
+			sum += d.get
+		}
+	}
+	
+	int standardDev(){
+		return 0;
+	}
+	
+	
+	Analysis(Data[] info, long t, int n){
+		data = info;
+		total = t;
+		num = n;
+	}
+	
+	
+
+}
---
+++ src/BackEnd/Data.java
@@ -0,0 +1,24 @@
+package BackEnd;
+
+/**
+ * Keeps track of the problem as well as the time 
+ * needed for a specific problem.
+ */
+public class Data {
+	private String problem;
+	private long time;
+	
+	Data(String s, long t){
+		problem = s;
+		time = t;
+	}
+	
+	long getTime(){
+		return time;
+	}
+	
+	String getProblem(){
+		return problem;
+	}
+	
+}
